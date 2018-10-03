dc301636ce3009b911849b4202754891b96efd73
Stephen Olson <stephenolson@gmail.com>
2018-10-03 20:04:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort.sort{|x, y| x.length <=> y.length}
+  array.sort{|x, y| x.length <=> y.length}
 end
 
 def swap_elements
